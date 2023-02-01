5cf560c14ef8bb548295217a914711a29c900894
AleixAlvarez22 <aalvarez22@jaumebalmes.net>
2023-02-01 20:46:41 +0100

    PabloA

--- PabloA/exercici1_A.py
+++ PabloA/exercici1_A.py
@@ -1,8 +1,5 @@
 
+#Creem la funció
 def calcul_Edat(naixement):
     any = 2023
     return any - naixement
-
-naixement = int(input('Quin any vas neixer? '))
-
-print(calcul_Edat(2003))
--- PabloA/exercici2_A.py
+++ PabloA/exercici2_A.py
@@ -1,10 +1,19 @@
+#Creem el diccionari
 myDict= {}
-myDict['Edat'] = 19
+
+#Anem afegint contingut al mateix
+myDict['Edat'] = 19 
 myDict['Estudis'] = 'Batxillerat'
 myDict['Mundials'] = 3
 myDict['Telefon'] = 689123341
 myDict['Germans'] = 2
 myDict['Pes'] = 65
+
+#Que imprimeixi per pantalla la longitud del diccionari
 print(len(myDict))
+
+#Valors del diccionari
 print(myDict.values())
+
+#Últim item del diccionari
 print(myDict.popitem())
